dcc225f84ab13ffd8942c8b32ec058dc94991516
pratik chirkute <pratik.chirkute@gmail.com>
2024-02-06 10:25:09 +0530

    added

---
+++ deliveryboy/deliveryboy/src/main/java/com/deliveryboy/deliveryboy/service/KafkaService.java
@@ -0,0 +1,22 @@
+package com.deliveryboy.deliveryboy.service;
+
+import com.deliveryboy.deliveryboy.config.AppConstants;
+import org.apache.kafka.common.protocol.types.Field;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaService {
+
+    private Logger logger = LoggerFactory.getLogger(KafkaService.class);
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+    public boolean updateLocation(String location){
+        this.kafkaTemplate.send(AppConstants.LOCATION_TOPIC_NAME,location);
+        this.logger.info("location produced");
+        return true;
+    }
+}
